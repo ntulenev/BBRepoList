ab504b7647a06d6704d4c9e67dc99a06befa0296
Nikita Tulenev <n1tulenev@gmail.com>
2026-02-01 21:37:42 +0100

    Refactors Bitbucket user tests.
    Simplifies and improves tests for the Bitbucket user model.
    Changes the focus of the tests to align with the updated model,
    specifically testing constructor property setting and default value handling.

--- BBRepoList.Tests/Models/BitbucketUser.Tests.cs
+++ BBRepoList.Tests/Models/BitbucketUser.Tests.cs
@@ -6,115 +6,35 @@ namespace BBRepoList.Tests.Models;
 
 public sealed class BitbucketUserTests
 {
-    [Fact(DisplayName = "Constructor allows null values")]
+    [Fact(DisplayName = "Constructor sets properties")]
     [Trait("Category", "Unit")]
-    public void ConstructorWhenArgumentsAreNullSetsProperties()
+    public void ConstructorWhenArgumentsAreValidSetsProperties()
     {
         // Arrange
-        string? displayName = null;
-        string? nickname = null;
-        string? uuid = null;
-        string? accountId = null;
+        var displayName = new UserName("Jane Doe");
+        var uuid = new BitbucketId("{uuid}");
 
         // Act
-        var user = new BitbucketUser(displayName, nickname, uuid, accountId);
+        var user = new BitbucketUser(uuid, displayName);
 
         // Assert
-        user.DisplayName.Should().BeNull();
-        user.Nickname.Should().BeNull();
-        user.Uuid.Should().BeNull();
-        user.AccountId.Should().BeNull();
+        user.DisplayName.Should().Be(displayName);
+        user.Uuid.Should().Be(uuid);
     }
 
-    [Fact(DisplayName = "Constructor trims non-empty values")]
+    [Fact(DisplayName = "Constructor uses default display name when null")]
     [Trait("Category", "Unit")]
-    public void ConstructorWhenArgumentsContainWhitespaceTrimsValues()
+    public void ConstructorWhenDisplayNameIsNullSetsDefault()
     {
         // Arrange
-        var displayName = "  Jane Doe  ";
-        var nickname = "  jdoe  ";
-        var uuid = "  123  ";
-        var accountId = "  acc-1  ";
+        var displayName = new UserName(null);
+        var uuid = new BitbucketId("{uuid}");
 
         // Act
-        var user = new BitbucketUser(displayName, nickname, uuid, accountId);
+        var user = new BitbucketUser(uuid, displayName);
 
         // Assert
-        user.DisplayName.Should().Be("Jane Doe");
-        user.Nickname.Should().Be("jdoe");
-        user.Uuid.Should().Be("123");
-        user.AccountId.Should().Be("acc-1");
-    }
-
-    [Fact(DisplayName = "Constructor throws when display name is whitespace")]
-    [Trait("Category", "Unit")]
-    public void ConstructorWhenDisplayNameIsWhitespaceThrowsArgumentException()
-    {
-        // Arrange
-        var displayName = " ";
-        var nickname = "jdoe";
-        var uuid = "123";
-        var accountId = "acc-1";
-
-        // Act
-        Action act = () => _ = new BitbucketUser(displayName, nickname, uuid, accountId);
-
-        // Assert
-        act.Should()
-            .Throw<ArgumentException>();
-    }
-
-    [Fact(DisplayName = "Constructor throws when nickname is whitespace")]
-    [Trait("Category", "Unit")]
-    public void ConstructorWhenNicknameIsWhitespaceThrowsArgumentException()
-    {
-        // Arrange
-        var displayName = "Jane Doe";
-        var nickname = "  ";
-        var uuid = "123";
-        var accountId = "acc-1";
-
-        // Act
-        Action act = () => _ = new BitbucketUser(displayName, nickname, uuid, accountId);
-
-        // Assert
-        act.Should()
-            .Throw<ArgumentException>();
-    }
-
-    [Fact(DisplayName = "Constructor throws when uuid is whitespace")]
-    [Trait("Category", "Unit")]
-    public void ConstructorWhenUuidIsWhitespaceThrowsArgumentException()
-    {
-        // Arrange
-        var displayName = "Jane Doe";
-        var nickname = "jdoe";
-        var uuid = "\t";
-        var accountId = "acc-1";
-
-        // Act
-        Action act = () => _ = new BitbucketUser(displayName, nickname, uuid, accountId);
-
-        // Assert
-        act.Should()
-            .Throw<ArgumentException>();
-    }
-
-    [Fact(DisplayName = "Constructor throws when account id is whitespace")]
-    [Trait("Category", "Unit")]
-    public void ConstructorWhenAccountIdIsWhitespaceThrowsArgumentException()
-    {
-        // Arrange
-        var displayName = "Jane Doe";
-        var nickname = "jdoe";
-        var uuid = "123";
-        var accountId = "\r\n";
-
-        // Act
-        Action act = () => _ = new BitbucketUser(displayName, nickname, uuid, accountId);
-
-        // Assert
-        act.Should()
-            .Throw<ArgumentException>();
+        user.DisplayName.Value.Should().Be("<N/A>");
+        user.Uuid.Should().Be(uuid);
     }
 }
