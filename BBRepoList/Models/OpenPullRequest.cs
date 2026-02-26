ab888716d04d38d66a4a78e6d2ac28ab467dafe7
Nikita Tulenev <n1tulenev@gmail.com>
2026-02-26 17:09:40 +0100

    Add xmldoc for OpenPullRequest

--- BBRepoList/Models/OpenPullRequest.cs
+++ BBRepoList/Models/OpenPullRequest.cs
@@ -1,5 +1,12 @@
 namespace BBRepoList.Models;
 
+/// <summary>
+/// Lightweight open pull request projection used during PR detail loading.
+/// </summary>
+/// <param name="Id">Pull request identifier within repository scope.</param>
+/// <param name="Title">Pull request title.</param>
+/// <param name="CreatedOn">Pull request creation timestamp.</param>
+/// <param name="AuthorId">Pull request author identifier when available.</param>
 internal readonly record struct OpenPullRequest(
     int Id,
     string Title,
